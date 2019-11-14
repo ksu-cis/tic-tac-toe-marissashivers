f9233327773d48de1dcd69bccd10ddf0e899ae50
Marissa Shivers <mxs@ksu.edu>
2019-11-14 15:36:25 -0600

    in class

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// global variable = document = access to DOM
+var currentTurn = "X";
+
+var turnElement = document.getElementById("turn"); // get the paragraph element
+turnElement.innerText = "Player " + currentTurn + "'s Turn.";
+//STRING TEMPLATE: `Player ${currentTurn}'s Turn.`
+
+// Add event listener to all squares in the board
+var squares = document.getElementsByClassName("square");
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", onClick);
+}
+
+function onClick(event) {
+    event.preventDefault(); // keeps it from doing default, want if we are overriding (get in habit of doing it)
+    event.target.innerText = currentTurn; // object that event is occuring on, equivalent to sender in C#
+    nextTurn();
+}
+
+function nextTurn() {
+    turnElement.innerText = "Player " + currentTurn + "'s Turn.";
+}
+
